bf316ee1b09ac9e752f9e2a54bd965de1a9511c1
Harry Watkins <harryspacewatkins@gmail.com>
2019-09-19 16:46:42 +0100

    Added buttons to my game and a way to take the payer name via text field rather than prompts

--- main.js
+++ main.js
@@ -1,64 +1,52 @@
-function generateComputerMove(){
-    var randomNumber = Math.random();
-    if (randomNumber <= 0.33 ){
-        return "rock";
-    } else if(randomNumber <= 0.66){
-        return "paper";
-    } else{
-        return "scissors";
-    }
+function generateComputerMove() {
+  var randomNumber = Math.random();
+  if (randomNumber <= 0.33) {
+    return "rock";
+  } else if (randomNumber <= 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-function checkWinner(move1, name1, move2, name2){
-    
-    if (move1 == move2){
-        console.log("Result is a draw!")
-    } 
-    else if (move2 == "rock"){
-    if (move1 == "paper"){
-        console.log(name1 + " " + "wins")
-    } else if (move1 === 'scissors') {
-        console.log(name2 + "wins")
-    }else {
-        console.log(name1 + " " + 'move is invalid')
-    }
-
-} else if (move2 == "paper"){
-    if (move1 == "scissors"){
-        console.log(name1 + " " + "wins")
-    } else if (move1 === 'rock'){
-        console.log(name2 + "wins")
+function checkWinner(move1, name1, move2, name2) {
+  if (move1 == move2) {
+    console.log("Result is a draw!");
+  } else if (move2 == "rock") {
+    if (move1 == "paper") {
+      console.log(name1 + " " + "wins");
+    } else if (move1 === "scissors") {
+      console.log(name2 + "wins");
     } else {
-        console.log(name1 + "" + 'move is invalid')
+      console.log(name1 + " " + "move is invalid");
     }
-
-} else if(move2 == "scissors"){
-    if (move1 == "rock"){
-        console.log(name1 + "" + "wins")
-    } else if (move1 === 'paper'){
-        console.log(name2 + "wins")
+  } else if (move2 == "paper") {
+    if (move1 == "scissors") {
+      console.log(name1 + " " + "wins");
+    } else if (move1 === "rock") {
+      console.log(name2 + "wins");
     } else {
-        console.log(name1 + "" + 'move is invalid')
+      console.log(name1 + "" + "move is invalid");
     }
-
-} else {
-    console.log(name2 + "move is invalid")
+  } else if (move2 == "scissors") {
+    if (move1 == "rock") {
+      console.log(name1 + "" + "wins");
+    } else if (move1 === "paper") {
+      console.log(name2 + "wins");
+    } else {
+      console.log(name1 + "" + "move is invalid");
     }
+  } else {
+    console.log(name2 + "move is invalid");
+  }
 }
 
-var isGamePlaying = true;
+var playerName;
 
-while (isGamePlaying){
-    var computerMove = generateComputerMove();
-    var computerName = "CPU ";
-    var playerName = prompt("Player 1 what is your name?");
-    var playerMove = prompt("Player 1 what is your move");
-
-
-    checkWinner(playerMove, playerName, computerMove, computerName)
-
-    isGamePlaying = confirm("Play again?");
+function playGame(playerMove) {
+  var computerMove = generateComputerMove();
+  var computerName = "CPU ";
+  var input = document.getElementById("name");
+  playerName = input.value;
+  checkWinner(playerMove, playerName, computerMove, computerName);
 }
-
-
-
